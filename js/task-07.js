40e20a47c1148806f01a75b2a003fe96c86372bd
Heveliev-destress <tolik05.03@ukr.net>
2022-12-10 13:44:39 +0200

    small change

--- js/task-07.js
+++ js/task-07.js
@@ -2,5 +2,7 @@ const inputHtml = document.querySelector('#font-size-control');
 const spanHtml = document.querySelector('#text');
 
 inputHtml.addEventListener('input', (event)=>{
-    
+
+
+console.log('11122333');
 })
