e9a2b8f5188d2a9da4588ee6f876771b2a2ba677
Heveliev-destress <tolik05.03@ukr.net>
2022-12-11 12:28:57 +0200

    add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -5,27 +5,33 @@ const divHtml = document.querySelector('#boxes');
 
 
 const inputHtml = document.querySelector('#controls').children[0];
-const btnCreate = document.querySelector('#controls').children[1];
-const btnDestroy = document.querySelector('#controls').children[2];
-let value = 0;
-
-// inputHtml.addEventListener('change',(evt)=>{
-//   value = Number(evt.target.value);
-//   })
-
-let size = 30;
-const patern = '<div style="display:flex; weight:30; height:30; background-color:tomato;"></div>'
-btnCreate.addEventListener('click', createBoxes)
-
-function createBoxes() {
- 
-
-  for(let i = 1;i <= 2;i += 1 ){
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
 
 
-divHtml.insertAdjacentHTML('beforeend',patern);
+console.log(btnDestroy)
 
 
-  }
+btnCreate.addEventListener("click", createFoo);
+btnDestroy.addEventListener("click", destroyFoo);
+
+
+function createFoo() {
+  let amount = + document.querySelector("#controls input").value;
+  createDiv(amount);
 }
 
+function createDiv(amount) {
+  const basSize = 30;
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < amount; i += 1) {
+    let size = basSize + i * 10;
+    let div = document.createElement("div");
+    div.style.cssText = `width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()}`;
+    fragment.appendChild(div);
+  }
+  boxes.appendChild(fragment);
+}
+function destroyFoo() {
+  boxes.innerHTML = "";
+}
